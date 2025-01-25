b3365678ed86b67aabac7648f592257202d7c0bd
zmantasha <mantasha@wordscloud.in>
2025-01-25 10:08:40 +0530

    Update page.tsx

--- app/account/signup/page.tsx
+++ app/account/signup/page.tsx
@@ -136,7 +136,7 @@ export default function SignUpPage() {
               </Link>
             </p>
           </form>
-          <button onClick={handleGoogleLogin} className={styles.googleButton}>
+{/*           <button onClick={handleGoogleLogin} className={styles.googleButton}>
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 48 48" className={styles.googleIcon}>
         <path fill="#FFC107" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12c0-6.627,5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24c0,11.045,8.955,20,20,20c11.045,0,20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"></path>
         <path fill="#FF3D00" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"></path>
@@ -144,7 +144,7 @@ export default function SignUpPage() {
         <path fill="#1976D2" d="M43.611,20.083H42V20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"></path>
       </svg>
       <span>Login with Google</span>
-    </button>
+    </button> */}
 
           {serverSuccessMessage && (
           <div className={styles.successMessage}>
