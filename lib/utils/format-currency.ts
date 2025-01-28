f16a8c01a0b4709d9a660af062a6cf86077d20cf
zmantasha <mantasha@wordscloud.in>
2025-01-28 14:59:11 +0530

    Update format-currency.ts

--- lib/utils/format-currency.ts
+++ lib/utils/format-currency.ts
@@ -8,24 +8,29 @@ export function formatCurrency(amount: number | undefined, currencyCode: string)
       currency: currencyCode || 'USD',
     }).format(0);
   }
-    // Check if the currency symbol is already cached
-    let symbol = currencyCache[currencyCode];
 
-    if (!symbol) {
-      // If not cached, find the currency symbol and store it in the cache
-      const currency = currencies.find((c) => c.value === currencyCode);
-      symbol = currency?.symbol || "$";
-      currencyCache[currencyCode] = symbol;
-    }
-  // const currency = currencies.find((c) => c.value === currencyCode);
-  // console.log("currency:", currency)
-  // const symbol = currency?.symbol || "$";
-  // console.log("symbol:", symbol)
-  return new Intl.NumberFormat('en-US', {
+  // Check if the currency symbol is already cached
+  let symbol = currencyCache[currencyCode];
+  if (!symbol) {
+    // If not cached, find the currency symbol and store it in the cache
+    const currency = currencies.find((c) => c.value === currencyCode);
+    symbol = currency?.symbol || "$";
+    currencyCache[currencyCode] = symbol;
+  }
+
+  // Format the amount
+  let formattedCurrency = new Intl.NumberFormat('en-US', {
     style: 'currency',
     currency: currencyCode || 'USD',
     currencyDisplay: 'symbol',
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(amount).replace(/[A-Z]{3}/, symbol);
+  }).format(amount);
+
+  // Replace only the INR symbol with 'Rs'
+  if (currencyCode === 'INR') {
+    formattedCurrency = formattedCurrency.replace('₹', 'Rs');
+  }
+
+  return formattedCurrency;
 }
