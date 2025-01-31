42a6367a2ad5e6e53072ae34a14022bfb15a9fff
zmantasha <mantasha@wordscloud.in>
2025-01-31 17:28:50 +0530

    Add files via upload

---
+++ app/google/callback/page.tsx
@@ -0,0 +1,26 @@
+"use client";
+
+import { useEffect } from "react";
+import { useRouter, useSearchParams } from "next/navigation";
+import Cookies from "js-cookie";
+
+export default function GoogleCallbackPage() {
+  const searchParams = useSearchParams();
+  const router = useRouter();
+
+  useEffect(() => {
+    const token = searchParams.get("token");
+
+    if (token) {
+      // Store token in a cookie (valid for 1 day)
+      Cookies.set("accessToken", token, { expires: 1, path: "/", secure: true, sameSite: "None" });
+
+      // Redirect smoothly without full reload
+      router.replace("/user/myinvoice");
+    } else {
+      router.replace("/account/login");
+    }
+  }, [searchParams, router]);
+
+  return <p>Processing Google login...</p>;
+}
