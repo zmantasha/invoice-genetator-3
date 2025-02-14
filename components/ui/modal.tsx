e53a4429a2f73b0458fe0403cecdfb974cc730d6
zmantasha <mantasha@wordscloud.in>
2025-02-14 14:57:36 +0530

    Update modal.tsx

--- components/ui/modal.tsx
+++ components/ui/modal.tsx
@@ -10,13 +10,14 @@ export const Modal: React.FC<ModalProps> = ({ open, onClose, children }) => {
   if (!open) return null;
 
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center">
-  <div className="bg-white p-5 rounded-lg w-[500px] relative">
-    <button className="absolute top-2 right-4 text-xl cursor-pointer" onClick={onClose}>
-      &times;
-    </button>
-    {children}
-  </div>
-</div>
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center px-4">
+      <div className="bg-white p-5 rounded-lg w-[90%] max-w-[500px] sm:max-w-[400px] relative">
+        <button className="absolute top-2 right-4 text-xl cursor-pointer" onClick={onClose}>
+          &times;
+        </button>
+        {children}
+      </div>
+    </div>
   );
 };
+
